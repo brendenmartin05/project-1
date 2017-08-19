2e51251c34a9974451b7de9a7e8b32573728448f
brendenmartin05 <brendenmartin1987@yahoo.com>
2017-08-18 20:21:33 -0700

    fixed project, ready for review

---
+++ js/quotes.js
@@ -0,0 +1,62 @@
+var quotes = [
+	{
+		quote: 'I\'m also honored to have the greatest temperament that anybody has.',
+		source: 'Donald Trump',
+		citation: 'Campaign Rally',
+		year: 2016
+	},
+	{
+		quote: 'You know I\’m automatically attracted to beautiful — I just start kissing them. It\’s like a magnet. Just kiss. I don\’t even wait. And when you\’re a star, they let you do it. You can do anything. Grab them by the p**sy. You can do anything.',
+		source: 'Donald Trump',
+		citation: 'Access Hollywood Interview',
+		year: 2005
+	},
+	{
+		quote: 'That makes me smart.',
+		source: 'Donald Trump',
+		citation: 'Presidential Debate',
+		year: 2016
+	},
+	{
+		quote: 'I\'ve had a beautiful, I\'ve had a flawless campaign. You\'ll be writing books about this campaign',
+		source: 'Donald Trump',
+		citation: 'Presidential Campaign Trail',
+		year: 2016
+	},
+	{
+		quote: 'I alone can fix it',
+		source: 'Donald Trump',
+		citation: 'RNC 2016',
+		year: 2016
+	},
+	{
+		quote: 'Change will not come if we wait for some other person or some other time. We are the ones we\'ve been waiting for. We are the change that we seek.',
+		source: 'Barack Obama',
+		citation: 'State of the Union',
+		year: 2012
+	},
+	{
+		quote: 'If you\'re walking down the right path and you\'re willing to keep walking, eventually you\'ll make progress.',
+		source: 'Barack Obama',
+		citation: 'Press Corps',
+		year: 2014
+	},
+	{
+		quote: 'You know, my faith is one that admits some doubt.',
+		source: 'Barack Obama',
+		citation: 'Jimmy Kimmel Live',
+		year: 2009
+	},
+	{
+		quote: 'I think when you spread the wealth around it\'s good for everybody.',
+		source: 'Barack Obama',
+		citation: 'White House Lawn',
+		year: 2010
+	},
+	{
+		quote: 'The United States is not, and never will be, at war with Islam.',
+		source: 'Barack Obama',
+		citation: 'State of the Union',
+		year: 2014
+	}
+];
--- js/script.js
+++ js/script.js
@@ -1,66 +1,3 @@
-var quotes = [
-	{
-		quote: 'I\'m also honored to have the greatest temperament that anybody has.',
-		source: 'Donald Trump',
-		citation: 'Campaign Rally',
-		year: 2016
-	},
-	{
-		quote: 'You know I\’m automatically attracted to beautiful — I just start kissing them. It\’s like a magnet. Just kiss. I don\’t even wait. And when you\’re a star, they let you do it. You can do anything. Grab them by the p**sy. You can do anything.',
-		source: 'Donald Trump',
-		citation: 'Access Hollywood Interview',
-		year: 2005
-	},
-	{
-		quote: 'That makes me smart.',
-		source: 'Donald Trump',
-		citation: 'Presidential Debate',
-		year: 2016
-	},
-	{
-		quote: 'I\'ve had a beautiful, I\'ve had a flawless campaign. You\'ll be writing books about this campaign',
-		source: 'Donald Trump',
-		citation: 'Presidential Campaign Trail',
-		year: 2016
-	},
-	{
-		quote: 'I alone can fix it',
-		source: 'Donald Trump',
-		citation: 'RNC 2016',
-		year: 2016
-	},
-	{
-		quote: 'Change will not come if we wait for some other person or some other time. We are the ones we\'ve been waiting for. We are the change that we seek.',
-		source: 'Barack Obama',
-		citation: 'State of the Union',
-		year: 2012
-	},
-	{
-		quote: 'If you\'re walking down the right path and you\'re willing to keep walking, eventually you\'ll make progress.',
-		source: 'Barack Obama',
-		citation: 'Press Corps',
-		year: 2014
-	},
-	{
-		quote: 'You know, my faith is one that admits some doubt.',
-		source: 'Barack Obama',
-		citation: 'Jimmy Kimmel Live',
-		year: 2009
-	},
-	{
-		quote: 'I think when you spread the wealth around it\'s good for everybody.',
-		source: 'Barack Obama',
-		citation: 'White House Lawn',
-		year: 2010
-	},
-	{
-		quote: 'The United States is not, and never will be, at war with Islam.',
-		source: 'Barack Obama',
-		citation: 'State of the Union',
-		year: 2014
-	}
-];
-
 // Declaring global variables
 var quote;
 var source;
@@ -70,7 +7,7 @@ var message;
 
 // Helper print function
 function print(message){
-	outputDiv = document.getElementById('quote-box');
+	var outputDiv = document.getElementById('quote-box');
 	outputDiv.innerHTML = message;
 }
 
@@ -110,12 +47,12 @@ function printQuote(){
 		message += '</span><span class="year">' + quoteDetails.year;
 		message += '</span></p>';
 	}
+	print(message);
 }
 
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
   document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-getRandomQuote();
 printQuote();
-print(message);
+
